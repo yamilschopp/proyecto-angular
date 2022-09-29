5d5d34b4566dedff47260419ce559511a82a3b0d
yamil schopp <yamilschopp@gmail.com>
2022-09-29 18:35:15 -0300

    inclusión estudiantes

--- src/app/components/contenido/contenido.component.ts
+++ src/app/components/contenido/contenido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from 'src/app/models/estudiantes';
 
 @Component({
   selector: 'app-contenido',
@@ -6,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contenido.component.css']
 })
 export class ContenidoComponent implements OnInit {
+
+  public ESTUDIANTES: Array<Estudiante> = [
+    {
+      legajo: 254,
+      nombre: 'Yamil',
+      apellido: 'Schopp',
+      dni: 35752721,
+      curso: 'Javascript',
+    },
+    {
+      legajo: 128,
+      nombre: 'Nicolas',
+      apellido: 'Schopp',
+      dni: 3356782,
+      curso: 'Desarrollo Web',
+    },
+    {
+      legajo: 100,
+      nombre: 'Romina',
+      apellido: 'Balbin',
+      dni: 30567898,
+      curso: 'Desarrollo Full Stack',
+    },
+    {
+      legajo: 250,
+      nombre: 'Paola',
+      apellido: 'Gimenez',
+      dni: 31256623,
+      curso: 'Javascript',
+    },
+    {
+      legajo: 180,
+      nombre: 'Marcos',
+      apellido: 'Martin',
+      dni: 35655215,
+      curso: 'Desarrollo Web',
+    },
+    {
+      legajo: 180,
+      nombre: 'Matias',
+      apellido: 'josviack',
+      dni: 32346987,
+      curso: 'Phyton',
+    },
+  ]
 
   constructor() { }
 
---
+++ src/app/models/estudiantes.ts
@@ -0,0 +1,7 @@
+export interface Estudiante {
+    legajo: number;
+    nombre: string;
+    apellido: string;
+    dni: number;
+    curso: string;
+}
